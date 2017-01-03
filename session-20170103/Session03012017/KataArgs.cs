affc596b42c436cf89867e0f64b4fee5c4d13683
Jan Voracek <jan@voracek.net>
2017-01-03 10:00:00 +0000

    Args

---
+++ session-20170103/Session03012017/KataArgs.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session03012017
+{
+    public class KataArgs
+    {
+        private readonly IDictionary<string, string> args = new Dictionary<string, string>();
+
+        public void ParseArgs(IReadOnlyCollection<string> args, IReadOnlyDictionary<string, string> schema = null)
+        {
+            for (var i = 0; i < args.Count; i++)
+            {
+                var key = args.ElementAt(i).Substring(1);
+                if (isEnabledFlag(schema, key))
+                    continue;
+                var value = isValidValue(args, i) ? args.ElementAt(++i) : string.Empty;
+                this.args.Add(key, value);
+            }
+        }
+
+        private static bool isValidValue(IReadOnlyCollection<string> args, int i)
+        {
+            return i + 1 < args.Count && !args.ElementAt(i + 1).StartsWith("-");
+        }
+
+        private static bool isEnabledFlag(IReadOnlyDictionary<string, string> schema, string key)
+        {
+            return schema != null && !schema.ContainsKey(key);
+        }
+
+        public int Size => args.Count;
+
+        public string GetFlagValue(string flag)
+        {
+            if (args.ContainsKey(flag))
+            {
+                return args[flag];
+            }
+            return null;
+        }
+    }
+}
---
+++ session-20170103/Session03012017Tests/KataArgsTests.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Session03012017;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session03012017.Tests
+{
+    [TestClass()]
+    public class KataArgsTests
+    {
+        [TestMethod()]
+        public void emptyArgsHasZeroSetFlags()
+        {
+            var kataArgs = new KataArgs();
+            kataArgs.ParseArgs(new List<string>());
+            Assert.AreEqual(0, kataArgs.Size);
+        }
+
+        [TestMethod()]
+        public void oneArgSetsOneFlag()
+        {
+            var kataArgs = new KataArgs();
+            kataArgs.ParseArgs(new List<string>{ "-l" });
+            Assert.AreEqual(1, kataArgs.Size);
+        }
+
+        [TestMethod()]
+        public void oneArgFlagWithStringValueHasSizeOne()
+        {
+            var kataArgs = new KataArgs();
+            kataArgs.ParseArgs(new List<string> { "-l", "a" });
+            Assert.AreEqual(1, kataArgs.Size);
+        }
+        [TestMethod()]
+        public void twoArgFlagWithStringValueHasSizeTwo()
+        {
+            var kataArgs = new KataArgs();
+            kataArgs.ParseArgs(new List<string> { "-l", "a","-p", "8080" });
+            Assert.AreEqual(2, kataArgs.Size);
+        }
+
+        [TestMethod()]
+        public void oneFlagWithoutValueAndOneFlagWithValueHasSizeTwo()
+        {
+            var kataArgs = new KataArgs();
+            kataArgs.ParseArgs(new List<string> { "-l","-p", "8080" });
+            Assert.AreEqual(2, kataArgs.Size);
+        }
+
+        [TestMethod()]
+        public void portFlagHasCorrectValue()
+        {
+            var kataArgs = new KataArgs();
+            kataArgs.ParseArgs(new List<string> { "-p", "8080" });
+            Assert.AreEqual("8080", kataArgs.GetFlagValue("p"));
+        }
+
+        [TestMethod()]
+        public void secondPortFlagHasCorrectValue()
+        {
+            var kataArgs = new KataArgs();
+            kataArgs.ParseArgs(new List<string> { "-l", "-p", "8080" });
+            Assert.AreEqual("8080", kataArgs.GetFlagValue("p"));
+        }
+
+        [TestMethod()]
+        public void nonSetFlagReturnsNull()
+        {
+            var kataArgs = new KataArgs();
+            kataArgs.ParseArgs(new List<string> { "-p", "8080" });
+            Assert.AreEqual(null, kataArgs.GetFlagValue("l"));
+        }
+
+        [TestMethod()]
+        public void noArgumentsParsedWithEmptySchema()
+        {
+            var kataArgs = new KataArgs();
+            var schema = new Dictionary<string, string>();
+            kataArgs.ParseArgs(new List<string> { "-l" }, schema);
+            Assert.AreEqual(null, kataArgs.GetFlagValue("l"));
+        }
+    }
+}
