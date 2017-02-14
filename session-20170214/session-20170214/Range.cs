8215685639c20e5983b961f26f7d87362e7ef59b
emilrezanina <rezanina.emil@gmail.com>
2017-02-14 22:16:21 +0100

    Range

---
+++ session-20170214/session-20170214/Range.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace session_20170214
+{
+
+
+    public class Range
+    {
+        private readonly bool _closedFromLeft;
+        private readonly bool _closedFromRight;
+        private readonly int _start;
+        private readonly int _end;
+        private bool _isValid;
+
+        public Range(String range)
+        {
+            ValidateInterval(range);
+            if (!_isValid)
+            {
+                return;
+            }
+            _closedFromLeft = range.StartsWith("[");
+            _closedFromRight = range.EndsWith("]");
+
+            int indexOfSeparator = range.IndexOf(",", StringComparison.Ordinal);
+            if (indexOfSeparator == -1)
+            {
+                _isValid = false;
+                return;
+            }
+            _start = Convert.ToInt32(range.Substring(1, indexOfSeparator - 1));
+            _end = Convert.ToInt32(range.Substring(indexOfSeparator + 1, range.Length - 1 - (indexOfSeparator + 1)));
+        }
+
+        public bool IsValid()
+        {
+            return _isValid;
+        }
+
+        private void ValidateInterval(String range)
+        {
+            _isValid = "([".Contains(range.ElementAt(0)) &&
+                       "])".Contains(range.ElementAt(range.Length - 1)) &&
+                       IsBodyValid(range);
+        }
+
+        private bool IsBodyValid(String range)
+        {
+            bool separateMarkWas = false;
+            char last = ' ';
+            for (int i = 1; i < range.Length - 1; i++)
+            {
+                char current = range.ElementAt(i);
+                if (current == ',' && !separateMarkWas)
+                {
+                    separateMarkWas = true;
+                    continue;
+                }
+                if ((!Char.IsDigit(current) && current != ',' && current != ' ')
+                        || (current == ',' && separateMarkWas))
+                {
+                    return false;
+                }
+                last = current;
+            }
+            return separateMarkWas;
+        }
+    }
+}
---
+++ session-20170214/session-20170214Tests/RangeTests.cs
@@ -0,0 +1,85 @@
+﻿using NUnit.Framework;
+using session_20170214;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace session_20170214.Tests
+{
+    [TestFixture]
+    public class RangeTests
+    {
+        [Test]
+        public void ClosedFromLeftIntervalStringIsValid()
+        {
+            Range range = new Range("[2,6)");
+            Assert.IsTrue(range.IsValid());
+        }
+
+        [Test]
+        public void NotIntervalStringIsNotValid()
+        {
+            Range range = new Range("[2,");
+            Assert.IsFalse(range.IsValid());
+        }
+
+        [Test]
+        public void ClosedIntervalFromBothSidesStringIsValid()
+        {
+            Range range = new Range("[2, 3]");
+            Assert.IsTrue(range.IsValid());
+        }
+
+        [Test]
+        public void ClosedIntervalFromRightSideStringIsValid()
+        {
+            Range range = new Range("(2, 3]");
+            Assert.IsTrue(range.IsValid());
+        }
+
+        [Test]
+        public void IntervalWithIncorectCharStringIsNotValid()
+        {
+            Range range = new Range("(2(, 3]");
+            Assert.IsFalse(range.IsValid());
+        }
+
+        [Test]
+        public void IntervalWithTwoSeparatorIsNotValid()
+        {
+            Range range = new Range("(2,, 3]");
+            Assert.IsFalse(range.IsValid());
+        }
+        
+        [Test]
+        public void EmptyClosedIntervalIsNotValid()
+        {
+            Range range = new Range("[]");
+            Assert.IsFalse(range.IsValid());
+        }
+
+        [Test]
+        public void IntervalWithoutEndNumber()
+        {
+            Range range = new Range("[3]");
+            Assert.IsFalse(range.IsValid());
+        }
+
+        [Test]
+        public void IntervalWithThreeNumberIsNotValid()
+        {
+            Range range = new Range("[3,4,4]");
+            Assert.IsFalse(range.IsValid());
+        }
+
+        [Test]
+        public void IntervalStartWithDoubleDigitNumber()
+        {
+            Range range = new Range("[63,5]");
+            Assert.IsTrue(range.IsValid());
+        }
+
+    }
+}
