8cd557fe341dd32f84243912b1a83394a57d7d62
emilrezanina <rezanina.emil@gmail.com>
2017-03-07 21:59:59 +0100

    Range #2
    Work with regular expression

---
+++ session-20170307/session-20170307/Range.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.RegularExpressions;
+
+namespace session_20170307
+{
+    public class Range
+    {
+        private readonly string _pattern = @"^([<\[])([ ]*[-]?[0-9]+[ ]*)([;])([ ]*[-]?[0-9]+[ ]*)([>\]])$";
+        private readonly bool _isValid;
+
+        public bool IsOpenFromLeft { get; set; }
+        public bool IsOpenFromRight { get; set; }
+        public int LeftBoundary { get; set; }
+        public int RightBoundary { get; set; }
+
+        public Range(string range)
+        {
+            _isValid = Regex.IsMatch(range, _pattern);
+            
+            if (_isValid)
+            {
+                var matchs = Regex.Match(range, _pattern);
+                IsOpenFromLeft = matchs.Groups[1].Value.Equals("<");
+                IsOpenFromRight = matchs.Groups[5].Value.Equals(">");
+                LeftBoundary = int.Parse(matchs.Groups[2].Value.Trim());
+                RightBoundary= int.Parse(matchs.Groups[4].Value.Trim());
+            }
+        }
+
+
+        public bool IsValidRange()
+        {
+            return _isValid;
+        }
+
+    }
+}
---
+++ session-20170307/session-20170307Tests/RangeTests.cs
@@ -0,0 +1,130 @@
+﻿using NUnit.Framework;
+using session_20170307;
+
+namespace session_20170307Tests
+{
+    [TestFixture]
+    public class RangeTests
+    {
+        [Test]
+        public void ValidRangeStringIsValid()
+        {
+            var range = new Range("[1;5]");
+            Assert.IsTrue(range.IsValidRange());
+        }
+
+        [Test]
+        public void InvalidRangeStringIsInvalid()
+        {
+            var range = new Range("[1;5");
+            Assert.IsFalse(range.IsValidRange());
+        }
+
+        [Test]
+        public void ValidRangeWithSpaceIsValid()
+        {
+            var range = new Range("[1; 5]");
+            Assert.IsTrue(range.IsValidRange());
+        }
+
+        [Test]
+        public void RangeWithoutSeparatorIsInvalid()
+        {
+            var range = new Range("[1 5]");
+            Assert.IsFalse(range.IsValidRange());
+        }
+
+        [Test]
+        public void RangeWithSpaceBeforeFirstNumber()
+        {
+            var range = new Range("[ 1; 5]");
+            Assert.IsTrue(range.IsValidRange());
+        }
+
+        [Test]
+        public void RangeWithoutLeftBoundaryIsInvalid()
+        {
+            var range = new Range(" 1; 5]");
+            Assert.IsFalse(range.IsValidRange());
+        }
+
+        [Test]
+        public void RangeWithSpaceAfterNumberIsValid()
+        {
+            var range = new Range("[1 ; 5 ]");
+            Assert.IsTrue(range.IsValidRange());
+        }
+
+        [Test]
+        public void TwoMultipleDigitNumberIsValidAs()
+        {
+            var range = new Range("[51 ; 52 ]");
+            Assert.IsTrue(range.IsValidRange());
+        }
+
+        [Test]
+        public void BoundaryWithSpaceInsideIsNotValidAsBoundaryValue()
+        {
+            var range = new Range("[51 ; 5 2 ]");
+            Assert.IsFalse(range.IsValidRange());
+        }
+
+        [Test]
+        public void NegativeBoundaryValuesAreValid()
+        {
+            var range = new Range("[-51 ; 2 ]");
+            Assert.IsTrue(range.IsValidRange());
+            range = new Range("[51 ; -2 ]");
+            Assert.IsTrue(range.IsValidRange());
+        }
+
+        [Test]
+        public void MultipleLeftBoundaryIsInvalid()
+        {
+            var range = new Range("[[ 51; 52 ]");
+            Assert.IsFalse(range.IsValidRange());
+        }
+
+        [Test]
+        public void DoubleRightBoundaryIsInvalid()
+        {
+            var range = new Range("< 51; 52 ]]");
+            Assert.IsFalse(range.IsValidRange());
+        }
+
+        [Test]
+        public void RangeWithTwoSeparatorIsInvalid()
+        {
+            var range = new Range("< 51; 52;5]");
+            Assert.IsFalse(range.IsValidRange());
+        }
+
+        [Test]
+        public void RangeIsOpenFromLeft()
+        {
+            var range = new Range("< 51; 52]");
+            Assert.IsTrue(range.IsOpenFromLeft);
+        }
+
+        [Test]
+        public void RangeIsOpenFromRight()
+        {
+            var range = new Range("< 51; 52>");
+            Assert.IsTrue(range.IsOpenFromRight);
+        }
+
+        [Test]
+        public void LeftBoundaryIsCorrect()
+        {
+            var range = new Range("< 51; 52>");
+            Assert.AreEqual(51, range.LeftBoundary);
+        }
+
+        [Test]
+        public void RightBoundaryIsCorrect()
+        {
+            var range = new Range("< 51; 52>");
+            Assert.AreEqual(52, range.RightBoundary);
+        }
+    }
+}
