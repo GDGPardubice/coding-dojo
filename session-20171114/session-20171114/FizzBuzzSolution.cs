206816a4033bf9f81324a2d05fc180897eae1f59
emilrezanina <rezanina.emil@gmail.com>
2017-11-15 20:39:15 +0100

    FizzBuzz
    FizzBuzz - Visual Studio 2017/C#

---
+++ session-20171114/session-20171114/FizzBuzzSolution.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.Remoting.Messaging;
+using System.Text;
+
+namespace session_20171114
+{
+    public class FizzBuzzSolution
+    {
+        public const string FIZZ = "Fizz";
+        public const string BUZZ = "Buzz";
+
+        public static string FizzBuzz(int number)
+        {
+            StringBuilder result = new StringBuilder();
+
+            if (number % 3 == 0)
+                result.Append(FIZZ);
+            if (number % 5 == 0)
+                result.Append(BUZZ);
+            if (result.Length == 0)
+                result.Append(number);
+
+            return result.ToString();
+        }
+    }
+}
---
+++ session-20171114/session-20171114Tests/FizzBuzzSolutionTests.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework;
+using session_20171114;
+
+namespace session_20171114Tests
+{
+    [TestFixture]
+    public class FizzBuzzSolutionTests
+    {
+        [Test]
+        public void NumberOneReturnsOneAsString()
+        {
+            string expected = "1";
+            string actual = FizzBuzzSolution.FizzBuzz(1);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void NumberTwoReturnsTwoAsString()
+        {
+            string expected = "2";
+            string actual = FizzBuzzSolution.FizzBuzz(2);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void NumberFourReturnsFourAsString()
+        {
+            string expected = "4";
+            string actual = FizzBuzzSolution.FizzBuzz(4);
+            Assert.AreEqual(expected, actual);
+        }
+        
+        [TestCase(3)]
+        [TestCase(6)]
+        [TestCase(9)]
+        public void MultiplyOfThreeReturnsFizz(int number)
+        {
+            string expected = FizzBuzzSolution.FIZZ;
+            string actual = FizzBuzzSolution.FizzBuzz(number);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(5)]
+        [TestCase(10)]
+        public void MultiplyOfFiveReturnsBuzz(int number)
+        {
+            string expected = FizzBuzzSolution.BUZZ;
+            string actual = FizzBuzzSolution.FizzBuzz(number);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(15)]
+        [TestCase(30)]
+        public void NumberFifteenReturnsFizzBuzz(int number)
+        {
+            string expected = "FizzBuzz";
+            string actual = FizzBuzzSolution.FizzBuzz(number);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
